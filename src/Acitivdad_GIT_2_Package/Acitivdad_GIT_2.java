d06ece16ac72325bd5dcf76d509f41d87d616ff1
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2024-12-23 16:31:09 +0100

    Commit A2
    Buscar el número en la matriz

--- src/Acitivdad_GIT_2_Package/Acitivdad_GIT_2.java
+++ src/Acitivdad_GIT_2_Package/Acitivdad_GIT_2.java
@@ -1,11 +1,31 @@
 package Acitivdad_GIT_2_Package;
 
+import java.util.Scanner;
+
 public class Acitivdad_GIT_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("\nIntroduce un número para buscar en la matriz: ");
+        int numeroBuscado = scanner.nextInt();
+        
+        boolean encontrado = false;
+
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (matriz[i][j] == numeroBuscado) {
+                    
+                    System.out.println("Número encontrado en la posición: Fila " + (i + 1) + ", Columna " + (j + 1));
+                    encontrado = true; 
+                }
+            }
+        }
+        
+        
+        
 	}
 
 }
