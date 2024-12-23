4808a146520eed3dea94e0f35108d39ade1f87d9
DayronVarsendan <dayronvarsendan@gmail.com>
2024-12-23 16:14:07 +0100

    Commit D1
    Se importan las utilidades, se declara el random y se inicia la matriz

--- src/Acitivdad_GIT_2_Package/Acitivdad_GIT_2.java
+++ src/Acitivdad_GIT_2_Package/Acitivdad_GIT_2.java
@@ -1,10 +1,20 @@
 package Acitivdad_GIT_2_Package;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Acitivdad_GIT_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		int[][] matriz = new int[5][5];
+	    Random random = new Random();
+	    
+	    
+	    
 	}
 
 }
